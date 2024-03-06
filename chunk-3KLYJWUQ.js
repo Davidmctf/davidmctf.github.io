30c117e0e0111d7e2b59514bd5c964f4661aa39a
David <programador.web3@skytex.com.mx>
2024-03-06 08:27:39 -0600

    Auto-generated commit

---
+++ chunk-3KLYJWUQ.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-JZA326JC.js";import"./chunk-AP23MKR4.js";export{a as routes};
--- chunk-I2EMMGA4.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-X2KUVX7D.js";import"./chunk-DU3K6HJM.js";export{a as routes};
