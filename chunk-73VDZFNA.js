1b2f0cdb1881ed5169bf434b0bc9460b0bdcdcf0
David Muñoz Cruz <davidmctf@gmail.com>
2025-08-11 14:53:16 -0600

    Auto-generated commit

---
+++ chunk-73VDZFNA.js
@@ -0,0 +1,1 @@
+import{h as a}from"./chunk-6A4HCF35.js";import"./chunk-A7OBRWTZ.js";export{a as routes};
--- chunk-DIRWPAYK.js
+++
@@ -1,1 +0,0 @@
-import{h as a}from"./chunk-M626JH54.js";import"./chunk-A7OBRWTZ.js";export{a as routes};
