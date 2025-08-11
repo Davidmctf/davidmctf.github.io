8965c0a60ff5e2bc2100fa90647f06dca27fad43
David Muñoz Cruz <davidmctf@gmail.com>
2025-08-11 12:22:52 -0600

    fix(package): update

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -58,7 +58,7 @@ export class FooterComponent implements OnInit, OnDestroy {
       link.href =
         format === 'pdf'
           ? 'assets/cv_resume/david_cv.pdf'
-          : 'assets/cv_resume/david_cv.html';
+          : 'assets/cv_resume/david_cv_indeed.pdf';
       link.download = `David_Munoz_Cruz_CV.${format}`;
       link.setAttribute('download', `David_Munoz_Cruz_CV.${format}`);
       document.body.appendChild(link);
